76a607960e553e5900f2f825f872aad655c63f67
tehwenyi <teh.wenyi22@gmail.com>
2023-08-18 13:36:50 +0800

    Add publisher and subscriber classes for interacting with ROS

---
+++ scripts/publisher.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+import rospy
+from vision_msgs.msg import BoundingBox2DArray, BoundingBox2D
+from sensor_msgs.msg import Image, CameraInfo
+
+class Publisher():
+  """
+  A class for publishing various types of messages to ROS topics.
+
+  This class provides methods to initialize publishers and publish messages to specific ROS topics.
+
+  Args:
+    msg_formats_list (list): List of message formats to publish.
+
+  Attributes:
+    _publishers (dict): Dictionary to store ROS publishers for different message formats.
+  """
+  
+  @classmethod
+  def get_ros_topics(cls):
+    """
+    Get a dictionary of ROS topics and their associated message types.
+
+    Returns:
+        dict: A dictionary containing ROS topics as keys and message types as values.
+    """
+    return {
+      'bbox': ('/cv/bounding_box', BoundingBox2DArray),
+      'chip': ('/cv/target_chip', Image),
+      'depth_frame': ('/cv/depth_frame', Image),
+      'cam_info': ('/cv/camera_info', CameraInfo)
+    }
+  
+  def __init__(self, msg_formats_list):
+    """
+    Initialize the Publisher class instance.
+
+    Initializes ROS node, publishers, and blank messages for the specified message formats.
+
+    Args:
+        msg_formats_list (list): List of message formats to publish.
+    """
+    rospy.init_node('cv', anonymous=True)
+
+    ros_topics = self.get_ros_topics()
+    
+    self._publishers = {}
+    
+    for msg_format in msg_formats_list:
+      if msg_format in ros_topics.keys():
+        topic, msg_type = ros_topics[msg_format]
+        self._init_publisher(msg_format, topic, msg_type)
+        self._init_blank_msg(msg_format, msg_type)
+      else:
+          print(f"ERROR: Invalid msg_format '{msg_format}'. Only the following formats are accepted: {', '.join(ros_topics.keys())}")
+
+  def _init_publisher(self, msg_format, topic, msg_type):
+    self._publishers[msg_format] = rospy.Publisher(topic, msg_type, queue_size=1)
+    print("init", self._publishers[msg_format], topic)
+  
+  def _init_blank_msg(self, msg_format, msg_type):
+    init_msg = msg_type()
+    self._publishers[msg_format].publish(init_msg)
+
+  def _convert_ltrb_to_cwh(self, bbox):
+    """
+    Convert bounding box coordinates from ltrb to c, w, h where c is (xcentre, ycentre).
+
+    Args:
+        bbox (tuple): Bounding box coordinates in ltrb format.
+
+    Returns:
+        tuple: A tuple containing c (center), w (width), and h (height) values.
+    """
+    ltrb, _, _ = bbox
+    l, t, r, b = ltrb
+    xc = (r - l) / 2.0
+    yc = (b - t) / 2.0
+    return (xc, yc), r-l, b-t
+  
+  def pub_bbox(self, dets, timestamp, frame_id):
+    """
+    Publish bounding box data to the corresponding ROS topic.
+
+    Args:
+        dets (list): List of detected bounding boxes.
+        timestamp (str): Timestamp for the message.
+        frame_id (str): Frame identifier for the message.
+    """
+    b_msg = BoundingBox2DArray()
+    # pose2d_array = []
+    
+    for i, det in enumerate(dets):
+      c, w, h = self._convert_ltrb_to_cwh(det)
+      xc, yc = c
+      bbox2d = BoundingBox2D()
+      bbox2d.center.x = xc
+      bbox2d.center.y = yc
+      bbox2d.size_x = w
+      bbox2d.size_y = h
+      b_msg.boxes.append(bbox2d)
+
+    b_msg.header.stamp.secs = int(timestamp[:10])
+    b_msg.header.stamp.nsecs = int(timestamp[10:])
+    b_msg.header.frame_id = frame_id
+    
+    self._publishers['bbox'].publish(b_msg)
+    print("pub bbox")
+  
+  def pub_camera_info(self):
+    """
+    Publish camera information (placeholder method).
+    """
+    pass
+  
+  def pub_depth_frame(self, depth_frame, timestamp, frame_id):
+    """
+    Publish depth frame data to the corresponding ROS topic.
+
+    Args:
+        depth_frame (numpy.ndarray): Depth frame data.
+        timestamp (str): Timestamp for the message.
+        frame_id (str): Frame identifier for the message.
+    """
+    df_msg = Image()
+
+    df_msg.header.seq = 0
+    
+    df_msg.header.stamp.secs = int(timestamp[:10])
+    df_msg.header.stamp.nsecs = int(timestamp[10:])
+    df_msg.header.frame_id = frame_id
+    
+    h, w, _ = depth_frame.shape
+    df_msg.height = h
+    df_msg.width = w
+
+    df_msg.encoding = "bgr8"
+    df_msg.data = depth_frame.tobytes()
+    self._publishers['depth_frame'].publish(df_msg)
+    print("pub depth frame")
+  
+  def pub_chip(self, img_chip, timestamp, frame_id):
+    """
+    Publish image chip data to the corresponding ROS topic.
+
+    Args:
+        img_chip (numpy.ndarray): Image chip data.
+        timestamp (str): Timestamp for the message.
+        frame_id (str): Frame identifier for the message.
+    """
+    chip = Image()
+
+    chip.header.seq = 0
+    
+    chip.header.stamp.secs = int(timestamp[:10])
+    chip.header.stamp.nsecs = int(timestamp[10:])
+    chip.header.frame_id = frame_id
+    
+    h, w, _ = img_chip.shape
+    chip.height = h
+    chip.width = w
+
+    chip.encoding = "bgr8"
+    chip.data = img_chip.tobytes()
+    self._publishers['chip'].publish(chip)
+    print("pub chip")
---
+++ scripts/subscriber.py
@@ -0,0 +1,91 @@
+import rospy
+import numpy as np
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class Subscriber():
+  """
+  A class for subscribing to ROS image topics and extracting image and metadata.
+
+  This class provides methods to initialize the subscriber, handle image callbacks, and extract image and related information.
+
+  Args:
+      ros_topic (str): ROS topic name to subscribe to.
+
+  Attributes:
+      bridge (CvBridge): A CvBridge object to convert ROS images to OpenCV format.
+      _img_sub (rospy.Subscriber): The ROS subscriber object for image messages.
+      width (int): Width of the subscribed image.
+      height (int): Height of the subscribed image.
+      encoding (str): Image encoding format.
+      step (int): Number of bytes per row of the image.
+      color_img (numpy.ndarray): The color image extracted from the ROS message.
+      timestamp (rospy.Time): Timestamp of the received image.
+      frame_id (str): Frame identifier of the received image.
+  """
+  
+  def __init__(self, ros_topic):
+    """
+    Initialize the Subscriber class instance.
+
+    Initializes ROS node, CvBridge, and subscribes to the specified ROS topic.
+
+    Args:
+        ros_topic (str): ROS topic name to subscribe to.
+    """
+    rospy.init_node('cv_sub', anonymous=True)
+
+    self.bridge = CvBridge()
+    self._img_sub = rospy.Subscriber(ros_topic, Image, self._img_callback)
+
+    self.width = None
+    self.height = None
+    self.encoding = None
+    self.step = None
+    self.color_img = None
+    self.timestamp = None
+    self.frame_id = None
+  
+  def _img_callback(self, data):
+    """
+    Callback function for handling incoming image messages.
+
+    Converts the ROS image message to a color image array and extracts relevant information.
+
+    Args:
+        data (sensor_msgs.msg.Image): The incoming ROS image message.
+    """
+    try:
+      color_img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      self.color_img = color_img
+      
+      self.width = data.width
+      self.height = data.height
+      self.encoding = data.encoding
+      self.step = data.step
+
+      # img_data = np.frombuffer(data.data, dtype=np.uint8)
+      # self.color_img = img_data.reshape((self.height, self.width, -1))
+      self.timestamp = data.header.stamp
+      self.frame_id = data.header.frame_id
+    except CvBridgeError as e:
+      print(e)
+
+  def get_frame_info(self):
+    """
+    Get image and related information from the subscribed message.
+
+    Returns:
+        dict: A dictionary containing color_img, width, height, encoding, step, timestamp, and frame_id.
+    """
+    return {
+      'img': self.color_img,
+      'width': self.width,
+      'height': self.height,
+      'encoding': self.encoding,
+      'step': self.step,
+      'timestamp': self.timestamp,
+      'frame_id': self.frame_id
+      }
+
