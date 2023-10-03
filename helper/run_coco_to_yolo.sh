9ceb7658c43ef1b7c363c7be1c5815c68ddbb305
tehwenyi <teh.wenyi22@gmail.com>
2023-10-03 16:10:45 +0800

    Add resize_images to run_coco_to_yolo.sh for easy usage

--- helper/run_coco_to_yolo.sh
+++ helper/run_coco_to_yolo.sh
@@ -1,8 +1,13 @@
 IMG_FOLDER='/path/to/img/folder'
-JSON_PATH='/path/to/annotations/json'
-OUTPUT_PATH='path/to/output/non-tiled/folder'
-OUTPUT_TILED_PATH='/path/to/output/tiled/folder'
-FALSEPATH='/path/to/store/empty/tiled/images'
+JSON_PATH='/path/to/json/annotations.json'
+OUTPUT_PATH='/path/to/output/path'
+NS_PATH='/path/to/ns/path'
+OUTPUT_TILED_PATH='/path/to/tiled/output/path'
+
+# For resizing images
+WIDTH=1280
+HEIGHT=720
+OUTPUT_RESIZE_PATH='/path/to/resize/output/path'
 
 python coco_to_yolo.py \
   $IMG_FOLDER \
@@ -13,12 +18,19 @@ python coco_to_yolo.py \
 # Remember to delete "images" and "labels" from $OUTPUT_PATH
 python split_train_val_test.py \
   $OUTPUT_PATH \
-  $OUTPUT_PATH \
+  $OUTPUT_PATH
   --test True
 
 # Optional: Tiling
 python tiling.py \
   $OUTPUT_PATH \
   $OUTPUT_TILED_PATH \
-  --falsepath $FALSEPATH \
+  --negative-samples-path $NS_PATH \
   --size 640
+
+# Optional: Resize images
+python resize_images.py \
+  --input-dir $OUTPUT_PATH \
+  --output-dir $OUTPUT_RESIZE_PATH \
+  --width $WIDTH \
+  --height $HEIGHT
